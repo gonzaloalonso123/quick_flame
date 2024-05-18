519fdcade5b5d3446a7e7693f75328653e53c98f
Gonzalo Alonso Garcia-Amorena <gonzalo@soilbeat.com>
2024-05-18 23:25:34 +0200

    Nodecook API: Pushing changes.

--- functions/database/daos/projects.js
+++ functions/database/daos/projects.js
@@ -2,12 +2,49 @@ const db = require("../config");
 const { v4: uuidv4 } = require("uuid");
 const ProjectsRef = db.collection("Projects");
 
+const getAll = async () => {
+  const querySnapshot = await ProjectsRef.get();
+  const data = [];
+  querySnapshot.forEach((doc) => {
+    data.push(doc.data());
+  });
+  return data;
+};
 
+const getById = async (id) => {
+  const docRef = ProjectsRef.doc(id);
+  const doc = await docRef.get();
+  if (!doc.exists) {
+    return null;
+  }
+  const data = doc.data();
+  return data;
+};
+
+const create = async (data) => {
+  const id = uuidv4();
+  const docRef = db.collection("Projects").doc(id);
+  await docRef.set({...data, id});
+  const newDoc = await docRef.get();
+  return newDoc.data();
+};
+
+const update = async (id, data) => {
+  const docRef = ProjectsRef.doc(id);
+  await docRef.update(data);
+  const updatedDoc = await docRef.get();
+  return updatedDoc.data();
+};
+
+const del = async (id) => {
+  const docRef = ProjectsRef.doc(id);
+  await docRef.delete();
+};
   
 module.exports = {
-  
-  
-  
-  
-  
+  create,
+  getAll,
+  getById,
+  update,
+  del,
 }
--- functions/routes/projects.js
+++ functions/routes/projects.js
@@ -3,4 +3,54 @@ const router = express.Router();
 const Projects = require("../database/daos/projects");
 
 
+router.get("/", async (req, res) => {
+  try {
+    const item = await Projects.getAll();
+    res.status(200).json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const item = await Projects.getById(req.params.id);
+    res.status(200).json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const item = await Projects.create(req.body);
+    res.status(200).json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.patch("/:id", async (req, res) => {
+  try {
+    const item = await Projects.update(req.params.id, req.body);
+    res.status(200).json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const item = await Projects.del(req.params.id);
+    res.status(200).json(item);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
 module.exports = router;
