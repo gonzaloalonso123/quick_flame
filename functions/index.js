458e8452335330d79b1b1d915484f7ab2c922389
Gonzalo Alonso Garcia-Amorena <gonzalo@soilbeat.com>
2024-05-18 23:23:10 +0200

    v1

--- functions/index.js
+++ functions/index.js
@@ -1,32 +1,19 @@
-const express = require("express");
-const app = express();
-const cors = require("cors");
-const functions = require("firebase-functions");
-const authMiddleware = require("./database/middleware/authMiddleware");
-const custom = require("./custom");
-const Projects = require("./routes/projects");
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
 
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-app.use(authMiddleware);
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
 
-app.use("/custom", custom);
-
-app.get("/test", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.use("/projects", Projects);
-
-exports.app = functions.https.onRequest(app);
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
--- functions/routes/projects.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const Projects = require("../database/daos/projects");
-
-
-module.exports = router;
